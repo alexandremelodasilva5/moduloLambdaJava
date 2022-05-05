3732538d07d363889af3f7913f0711a0220b2ab9
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-05 14:24:05 -0300

    usando expressão lambda no comparator

--- appTestes/src/application/Program.java
+++ appTestes/src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.MyComparator;
@@ -10,13 +11,22 @@ public class Program {
 
 	public static void main(String[] args) {
 		List<Product> list = new ArrayList<>();
-		Product p1 = new Product("TV", 1000.0);
-		Product p2 = new Product("Note", 3000.0);
-		Product p3 = new Product("tablet", 2000.0);
-		list.add(p3);
-		list.add(p2);
-		list.add(p1);
-		list.sort(new MyComparator());
+		Product prod1 = new Product("TV", 1000.0);
+		Product prod2 = new Product("Note", 3000.0);
+		Product prod3 = new Product("tablet", 2000.0);
+		list.add(prod3);
+		list.add(prod2);
+		list.add(prod1);
+		
+		/*Comparator<Product> comp = new Comparator<Product>() {
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+		}*/
+		
+		
+		list.sort((p1,p2) ->  p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 		
 		for(Product p:list) {
 			System.out.println(p);
