95be82fd733efa8259aa8eee1759a85bd00e32ea
ALEXANDRE MELO DA SILVA <alexandremelodasilva5@gmail.com>
2022-05-05 16:19:41 -0300

    aula consumer

--- appTestes/src/application/Program.java
+++ appTestes/src/application/Program.java
@@ -3,15 +3,19 @@ package application;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Locale;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import entities.MyComparator;
 import entities.Product;
+import util.ProductConsumer;
 import util.ProductPredicate;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
 		Product prod1 = new Product("TV", 1000.0);
 		Product prod2 = new Product("Note", 3000.0);
@@ -37,16 +41,25 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Predicate<Product> pred = p -> p.getPrice() >= 100;
-		
+		//Predicate<Product> pred = p -> p.getPrice() >= 100;
 		//list.removeIf(p->p.getPrice()>100.0);
 		//list.removeIf(new ProductPredicate());
 		//list.removeIf(Product::staticProductPredicate);
 		//list.removeIf(Product::nonStaticProductPredicate);
-		list.removeIf(pred);
-		for(Product p:list) {
+		//list.removeIf(pred);
+		
+		//Consumer<Product> cons = p ->p.setPrice(p.getPrice()*1.1);
+		//list.forEach(new ProductConsumer());
+		//list.forEach(Product::staticPriceUpdate);
+		//list.forEach(Product::nonStaticPriceUpdate);
+		//list.forEach(cons);
+		list.forEach(p ->p.setPrice(p.getPrice()*1.1));
+		list.forEach(System.out::println);
+		/*for(Product p:list) {
 			System.out.println(p);
-		}
+		}*/
+		
+		
 	}
 
 }
